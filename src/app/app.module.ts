2fdf8bc9744917a7a2b63c13050eae12f7027def
Nick Boyd <nickjohnboyd@gmail.com>
2020-04-30 15:08:00 -0600

    initial components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+import { ScorecardComponent } from './scorecard/scorecard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    HomeComponent,
+    ScorecardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scorecard/scorecard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scorecard',
+  templateUrl: './scorecard.component.html',
+  styleUrls: ['./scorecard.component.scss']
+})
+export class ScorecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
