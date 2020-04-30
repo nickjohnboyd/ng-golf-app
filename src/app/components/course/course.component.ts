d8c4f1988fe9aa3570badfae5b669a4d0013ba46
Nick Boyd <nickjohnboyd@gmail.com>
2020-04-30 17:53:09 -0600

    options animation added

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,17 +1,71 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { Course } from 'src/app/models/course';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
+  animations: [
+    trigger('enterLeave', [
+      transition(':enter', [
+        style({
+          height: 0,
+          opacity: 0
+        }),
+        animate('0.5s ease-out', style({
+          height: '*',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          height: '*',
+          opacity: 1
+        }),
+        animate('0.5s ease-in', style({
+          height: 0,
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('openClose', [
+      state('open', style({
+        height: '*',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '*',
+        opacity: 0,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('0.35s')
+      ]),
+      transition('closed => open', [
+        animate('0.35s')
+      ])
+    ])  
+  ]
 })
 export class CourseComponent implements OnInit {
   @Input() course: Course;
+  isOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showOptions() {
+    this.isOpen = !this.isOpen;
   }
 
 }
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { CoursesService } from 'src/app/shared/courses.service';
 import { Course } from 'src/app/models/course';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
   courses: Course[];
